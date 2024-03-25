abae73c1b334521957c8d59b8b6d4c77f7638e00
khoaLe12 <112998676+khoaLe12@users.noreply.github.com>
2024-03-25 21:19:44 +0700

    up file

---
+++ Base.API/Controllers/UpFilesController.cs
@@ -0,0 +1,31 @@
+﻿using Base.Service.Common;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Base.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UpFilesController : ControllerBase
+    {
+        private readonly IUploadFile upload;
+        public UpFilesController(IUploadFile uploadFile)
+        {
+            upload = uploadFile;
+        }
+        [HttpPost]
+        public async Task<IActionResult> UploadFile(IFormFile? file)
+        {
+            if(file is not null && file.Length > 0)
+            {
+                var uploadResult = await upload.UploadImage(file);
+                if (uploadResult.Error is not null)
+                {
+                    return BadRequest();
+                }
+                return Ok(uploadResult.SecureUrl.ToString() + "   " + uploadResult.Url.ToString());
+            }
+            return BadRequest();
+        }
+    }
+}
